f8547f5e4894688d60a22fedbd85992b62302726
XANATORR <emrecan5386@gmail.com>
2026-05-13 12:39:04 +0300

    Bug Fixed

--- start_app.bat
+++ start_app.bat
@@ -6,5 +6,5 @@ echo Site adresi: http://127.0.0.1:5000/
 echo Sunucuyu kapatmak icin bu pencerede CTRL+C tuslayin.
 echo.
 start "" http://127.0.0.1:5000/
-python -m flask --app app run --host 127.0.0.1 --port 5000 --no-reload
+python -m flask --app app run --host 127.0.0.1 --port 5000 --debug
 pause
